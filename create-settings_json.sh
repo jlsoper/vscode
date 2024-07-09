54c8e17a507f99c42c96326f3bd6b98acd88d4bf
James <soapman22@frontier.com>
2024-07-08 23:31:12 -0500

    Mon 07/08/24 23:31 -- Alpine \ jsoper

---
+++ create-settings_json.sh
@@ -0,0 +1,27 @@
+#  Create settings.json file
+
+
+echo
+
+echo -n "Select which terminal type, to append: (1 = bash, 2 = ash): " ; read choice
+
+
+case $choice in
+
+  1)
+    cat json-settings.txt term-bash.txt > $USER_VSC/settings.json
+    ;;
+
+  2)
+    cat json-settings.txt term-ash.txt > $USER_VSC/settings.json
+    ;;
+
+  *)
+    echo
+    echo "Invalid input, exiting.."
+    echo
+    exit 1
+    ;;
+esac
+
+
